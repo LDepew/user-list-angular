2cf0937cc3148df2e640730840b99b5ad4a8792e
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-31 09:45:40 -0400

    add list and get by id function

---
+++ src/app/user/user.class.ts
@@ -0,0 +1,11 @@
+export class User {
+    id: number = 0;
+    username: string = ' ';
+    password: string = ' ';
+    firstName: string = ' ';
+    lastName: string = ' ';
+    phoneNumber: string = ' ';
+    email: string = ' ';
+    reviewer: boolean = false;
+    admin: boolean = false;
+}
